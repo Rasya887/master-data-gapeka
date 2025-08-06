61022374134e4e91732bc3feeb884a4ab3385e21
Rasya Rafandra <rrafandra2008@gmail.com>
2025-08-06 13:32:36 +0700

    Initial commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -8,9 +9,17 @@ export default defineConfig({
       input: [
         'resources/css/app.css',
         'resources/js/app.js',
+
+        // Tambahkan ini agar file Vue ke-load ke manifest
+        ...require('fast-glob').sync('resources/js/Pages/**/*.vue'),
       ],
       refresh: true,
     }),
     vue(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/js'),
+    },
+  },
 })
